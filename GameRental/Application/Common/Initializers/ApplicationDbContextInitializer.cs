c1b1e0314b4cdf2f570ff95e5d8062c361bd4273
Artur <artspiew@pg.edu.pl>
2024-12-01 17:48:30 +0100

    Init architecture

---
+++ GameRental/Application/Common/Initializers/ApplicationDbContextInitializer.cs
@@ -0,0 +1,12 @@
+﻿using Application.Common.Interfaces;
+
+namespace Application.Common.Initializers
+{
+    public class ApplicationDbContextInitializer(IApplicationDbContext context)
+    {
+        public async Task SeedSampleData()
+        {
+            await context.SaveChangesAsync();
+        }
+    }
+}
---
+++ GameRental/Application/Repositories/Interfaces/ICrudOperations.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories.Interfaces
+{
+    public interface ICrudOperations
+    {
+        bool Add();
+        bool Delete();
+        bool Update();
+        IBaseEntity GetById(int id);
+    }
+}
