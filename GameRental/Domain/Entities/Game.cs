47384c14559d3002e6c57ec75185adcae50d91ad
Artur <artspiew@pg.edu.pl>
2024-12-01 18:00:07 +0100

    Prepare entity

--- GameRental/Domain/Entities/Game.cs
+++ GameRental/Domain/Entities/Game.cs
@@ -1,7 +1,16 @@
-﻿namespace Domain
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Domain.Entities;
+
+namespace Domain
 {
     public class Game
     {
-
+        [Key] public int Id { get; set; }
+        [MaxLength(100)] public string Name { get; set; }
+        public int? GenreId { get; set; }
+        [ForeignKey("GenreId")]
+        public Genre? Genre { get; set; }
     }
 }
