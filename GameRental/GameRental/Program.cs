a2eade07bfcfb0ca0820aa40ae685081ed7f3861
ArturLog <artur.spiewak2967@gmail.com>
2024-12-17 16:44:21 +0100

    Change configuration

--- GameRental/GameRental/Program.cs
+++ GameRental/GameRental/Program.cs
@@ -1,20 +1,52 @@
 using GameRental.Data;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Localization;
 using Microsoft.EntityFrameworkCore;
+using Infrastructure.Data;
+using System.Globalization;
+using ApplicationDbContext = GameRental.Data.ApplicationDbContext;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(connectionString));
+//var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+//builder.Services.AddDbContext<ApplicationDbContext>(options =>
+//    options.UseSqlServer(connectionString));
+
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddInfrastructureServices();
+
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+
+builder.Services.AddLocalization();
+builder.Services.AddMvc().AddDataAnnotationsLocalization()
+    .AddViewLocalization(options => options.ResourcesPath = "Resources");
 
 builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ApplicationDbContext>();
-builder.Services.AddRazorPages();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("RequireAccount",
+        policy => policy.RequireRole("User"));
+});
+
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Pages/Account");
+});
 
 var app = builder.Build();
+
+var supportedCultures = new[] { new
+    CultureInfo("en"), new CultureInfo("pl")};
+
+app.UseRequestLocalization(new RequestLocalizationOptions
+{
+    DefaultRequestCulture = new RequestCulture("en"),
+    SupportedCultures = supportedCultures,
+    SupportedUICultures = supportedCultures
+});
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
