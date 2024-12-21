f7e1565dd88455ce8bc4e2dd9aaecac6a9d35502
ArturLog <artur.spiewak2967@gmail.com>
2024-12-21 18:53:21 +0100

    Create service for api

---
+++ GameRental/Application/Services/Classes/GameService.cs
@@ -0,0 +1,39 @@
+﻿using Application.Repositories.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services.Classes
+{
+    public class GameService
+    {
+        private readonly ICrudRepository<Game> _gameRepository;
+        public GameService(ICrudRepository<Game> gameRepository)
+        {
+            _gameRepository = gameRepository;
+        }
+
+        public async Task<IEnumerable<Game>> GetAllGames()
+        {
+            return await _gameRepository.ReadAll();
+        }
+
+        public async Task<Game?> GetGameById(int id)
+        {
+            return await _gameRepository.Read(id);
+        }
+
+        public async Task<Game> AddGame(Game game)
+        {
+            return await _gameRepository.Create(game);
+        }
+
+        public async Task<bool> UpdateGame(Game game)
+        {
+            return await _gameRepository.Update(game);
+        }
+
+        public async Task<bool> DeleteGame(int id)
+        {
+            return await _gameRepository.Delete(id);
+        }
+    }
+}
